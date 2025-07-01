8457ecec914505d109a83ae1436d48cdd6b8c426
DmitriiSA <letfun@rambler.ru>
2025-07-02 00:28:49 +0500

    1st comm terr 4

--- config.tf
+++ config.tf
@@ -37,6 +37,7 @@ resource "yandex_compute_instance" "default" {
   network_interface {
     index     = 1
     subnet_id = yandex_vpc_subnet.default-ru-central1-d.id
+    nat-ip-version= ipv4
   }
 
   metadata = {
