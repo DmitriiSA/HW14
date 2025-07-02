a601a0716c5f184f49408189dd76a4b8cded9da4
DmitriiSA <letfun@rambler.ru>
2025-07-02 23:46:53 +0500

    4st comm terrf

--- config.tf
+++ config.tf
@@ -44,7 +44,6 @@ resource "yandex_compute_instance" "build" {
       image_id = "fd8vmcue7aajpmeo39kk" # ОС Ubuntu 20.04 LTS
       size     =  7
       type     = "network-ssd"
-      zone     = "ru-central1-d"
     }
   }
 
@@ -79,12 +78,60 @@ resource "yandex_compute_instance" "build" {
   }
 }
 
-// Create a new Compute Disk.
+// Create a new Prod Instance ( Java, tomcat)
 //
-//resource "yandex_compute_disk" "boot-disk" {
-  //name     = "boot"
-  //size     =  7
-  //type     = "network-ssd"
-  //zone     = "ru-central1-d"
-//}
+resource "yandex_compute_instance" "prod" {
+  name        = "prod-instance"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-d"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 50
+  }
+
+  boot_disk {
+      initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # ОС Ubuntu 20.04 LTS
+      size     =  7
+      type     = "network-ssd"
+    }
+  }
+
+  network_interface {
+    index     = 1
+    subnet_id = yandex_vpc_subnet.default-ru-central1-d.id
+    //nat_ip_version = ipv4
+    nat = true
+    ipv4 = true
+  }
+
+  metadata = {
+    foo      = "bar"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+        user-data = <<-EOF
+      #cloud-config
+      package_update: true
+      package_upgrade: true
+      packages:
+        - default-jdk
+        - tomcat9
+      runcmd:
+        - while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done
+        - apt-get install -y wget
+        - wget http://${yandex_compute_instance.build.network_interface.0.nat_ip_address}/hello-1.0.war -O /var/lib/tomcat9/webapps/hello.war
+        - systemctl restart tomcat9
+      EOF
+  }
+  depends_on = [yandex_compute_instance.build]
+}
+output "build_instance_ip" {
+  value = yandex_compute_instance.build.network_interface.0.nat_ip_address
+}
+
+output "prod_instance_ip" {
+  value = yandex_compute_instance.prod.network_interface.0.nat_ip_address
+  description = "Access the application at: http://<prod_ip>:8080/hello/"
+}
 
